0ff8023e255bbbccaf0f46bd350bea419e66b999
Crucible Deploy <chris@dod.net>
2019-06-06 14:36:48 -0400

    api/controller/models for a crucible and a participant

---
+++ api/models/Participant.js
@@ -0,0 +1,47 @@
+/**
+ * Participant.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  primaryKey: 'address',
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    address: {
+      type: 'string',
+      required: true,
+      regex: /^0x[0-9a-f]{40}$/i
+    },
+    crucibleAddress: {
+      type: 'string',
+      required: true,
+      regex: /^0x[0-9a-f]{40}$/i
+    },
+    createdAt: {
+      type: 'number',
+      autoCreatedAt: true
+    },
+    updatedAt: {
+      type: 'number',
+      autoUpdatedAt: true
+    },
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
