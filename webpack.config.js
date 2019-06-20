72e2fa879ab35e7f50c3b3ad058d729f8d52ed29
Crucible Deploy <chris@dod.net>
2019-06-20 11:50:27 -0400

    cleaning up old sails init

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    entry: './assets/src/index.js'
+    entry: './client/js/index.js'
   },
   output: {
     path: __dirname + '/.tmp/public',
@@ -24,7 +24,7 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'assets/src/index.html'
+      template: 'client/js/index.html'
     })
   ]
 };
