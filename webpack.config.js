dfa51f46b5da0f34582788319ac6421e80f347f2
Crucible Deploy <chris@dod.net>
2019-06-20 12:49:28 -0400

    broke babel import

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
+
+import webpack from 'webpack';
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const precss = require('precss');
+const autoprefixer = require('autoprefixer');
+const path = require('path');
 
 module.exports = {
-  entry: {
-    entry: './client/js/index.js'
-  },
+  devtool: 'source-map',
+  entry: [
+    './client/js/index.js',
+    'webpack-hot-middleware/client?reload=true'
+  ],
   output: {
-    path: __dirname + '/.tmp/public',
-    filename: 'bundle.js'
+    path: path.join(__dirname, '/.tmp/public'),
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
@@ -18,13 +27,33 @@ module.exports = {
       {
         use: ['style-loader', 'css-loader'],
         test: /\.css$/
+      },
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader' // inject CSS to page
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run post css actions
+          options: {
+            plugins: () => // post css plugins, can be exported to postcss.config.js
+              [
+                precss,
+                autoprefixer
+              ]
+          }
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
       }
     ]
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'client/js/index.html'
-    })
+      template: 'client/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ]
 };
